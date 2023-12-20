db4dcffb954ec74f9b3155d41c570787c87b1449
hrushitha <hrushithachinthakayalc@gmail.com>
2023-12-20 19:13:46 +0530

    drowsiness-detection

---
+++ Minor_Project-main/README.md
@@ -0,0 +1,15 @@
+# Minor_Project
+
+shape_predictor_68_face_landmarks.dat is also required
+
+#Execution 
+
+1) download cmake (from official website or pip install cmake)
+2) download visual installer(microsoft website)
+3) install at system level and create a cmake project
+4) open developer command prompt  and install dlib(pip install dlib)
+4)now create a python directory and install:
+a)pip install imutilis
+b)pip install opencv2-head
+c)pip install numpy
+d)pip install pygame
---
+++ Minor_Project-main/drowsy/drowsy.py
@@ -0,0 +1,114 @@
+import cv2
+import dlib
+import numpy as np
+from imutils import face_utils
+import pygame
+
+cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+sleep = 0
+drowsy = 0
+active = 0
+yawn = 0
+status = ""
+color = (0, 0, 0)
+
+pygame.mixer.init()
+alarm_sound = pygame.mixer.Sound("alarm.mp3")
+
+def compute(ptA, ptB):
+    dist = np.linalg.norm(ptA - ptB)
+    return dist
+
+def blinked(a, b, c, d, e, f):
+    up = compute(b, d) + compute(c, e)
+    down = compute(a, f)
+    ratio = up / (2.0 * down)
+
+    if ratio > 0.25:
+        return 2
+    elif ratio > 0.21 and ratio <= 0.25:
+        return 1
+    else:
+        return 0
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+
+    face_frame = frame.copy()
+    alarm_playing = False
+
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+
+        cv2.rectangle(face_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        landmarks = predictor(gray, face)
+        landmarks = face_utils.shape_to_np(landmarks)
+
+        left_blink = blinked(landmarks[36], landmarks[37],
+                             landmarks[38], landmarks[41], landmarks[40], landmarks[39])
+        right_blink = blinked(landmarks[42], landmarks[43],
+                              landmarks[44], landmarks[47], landmarks[46], landmarks[45])
+
+        mouth_open = compute(landmarks[50], landmarks[58]) # Distance between mouth corners
+
+        if mouth_open > 25.0:
+            yawn += 1
+            if yawn > 10:
+                status = "Yawning :("
+                color = (0, 0, 255)
+        else:
+            yawn = 0
+
+        if left_blink == 0 or right_blink == 0 or yawn > 10:
+            sleep += 1
+            drowsy = 0
+            active = 0
+            if sleep > 6:
+                status = "SLEEPING !!!"
+                color = (255, 0, 0)
+                if not alarm_playing:
+                    pygame.mixer.Channel(0).play(alarm_sound)
+                    alarm_playing = True
+        elif left_blink == 1 or right_blink == 1:
+            sleep = 0
+            active = 0
+            drowsy += 1
+            if drowsy > 6:
+                status = "Drowsy !"
+                color = (0, 0, 255)
+        else:
+            drowsy = 0
+            sleep = 0
+            active += 1
+            if active > 6:
+                status = "Active :)"
+                color = (0, 255, 0)
+                if alarm_playing:
+                    pygame.mixer.Channel(0).stop()
+                    alarm_playing = False
+
+        cv2.putText(frame, status, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 3)
+
+        for n in range(0, 68):
+            (x, y) = landmarks[n]
+            cv2.circle(face_frame, (x, y), 1, (255, 255, 255), -1)
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Result of detector", face_frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
